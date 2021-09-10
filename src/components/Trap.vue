706ce2262ab04b341bedff927487f33662550f2b
Dylan Hobbs <dhobbs@tcd.ie>
2021-09-10 23:00:00 +0300

    Enable scan trap button on location page

--- src/components/Trap.vue
+++ src/components/Trap.vue
@@ -9,17 +9,17 @@
         <span class="font-weight-black">Distance: </span>{{ trap.distance }}
       </v-list-item-subtitle>
       <v-list-item-subtitle>
-        <span class="font-weight-bold">NZ ID: </span>{{ trap.nz_trap_id }}
+        <span class="font-weight-bold">QR ID: </span>{{ trap.qr_id }}
       </v-list-item-subtitle>
       <v-list-item-subtitle>
-        <span class="font-weight-bold">QR ID: </span>{{ 'Test-1234' }}
+        <span class="font-weight-bold">NZ ID: </span>{{ trap.nz_trap_id }}
       </v-list-item-subtitle>
     </v-list-item-content>
 
     <v-list-item-action class="align-baseline">
       <span>
         <v-icon class="mr-4" :style="style">mdi-navigation</v-icon>
-        <v-btn class="primary"> Scan Trap </v-btn>
+        <v-btn class="primary" @click="scan" :disabled="! trap.nz_trap_id"> Scan Trap </v-btn>
       </span>
     </v-list-item-action>
   </v-list-item>
@@ -30,6 +30,11 @@ export default {
   props: {
     trap: {
       type: Object
+    }
+  },
+  methods: {
+    scan () {
+      this.$store.dispatch('scanQR', { qr_id: this.trap.qr_id })
     }
   },
   computed: {
