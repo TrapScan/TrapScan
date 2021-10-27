eeac21cedf30428bd662659ef65d9165996321e8
Dylan Hobbs <dhobbs@tcd.ie>
2021-10-27 16:45:30 +0300

    Switching qr scanning again to one that supports tree shaking. This involves a change to how scanning works as we require a callback from the action to trigger the stop and starting of scanning. 30% reduction in bundle size is hard to say no to though

--- src/store/scan.js
+++ src/store/scan.js
@@ -52,7 +52,7 @@ function relativeBearing (p1, p2, heading) {
 
   const y = Math.sin(destLng - startLng) * Math.cos(destLat)
   const x = Math.cos(startLat) * Math.sin(destLat) -
-        Math.sin(startLat) * Math.cos(destLat) * Math.cos(destLng - startLng)
+    Math.sin(startLat) * Math.cos(destLat) * Math.cos(destLng - startLng)
   let brng = Math.atan2(y, x)
   brng = toDegrees(brng)
   brng = (brng + 360) % 360
@@ -78,60 +78,7 @@ const scanModule = {
   },
   mutations: {
     scanQR (state, data) {
-      if (data.qr_id === null) {
-        // Dealing with an unmapped trap here (from location screen)
-        scan.checkPCordByTrap(data.id).then((response) => {
 
-        })
-          .catch(() => {
-          // User not a coordinator for this trap, shouldn't happen, abort aggresivly
-            state.scanError = 'You don\'t have permission to do that'
-          })
-      } else {
-        // QR exists, trap may not
-        scan.scan(data.qr_id).then((response) => {
-          const trap = response.data
-          state.scannedTrap = trap
-          state.scannedQRID = data.qr_id
-          let pcord = false
-          scan.checkPCord().then((response) => {
-            pcord = true
-            state.pcordData = response.data
-          }).catch(() => {
-            pcord = false
-          }).finally(() => {
-            const currentUser = data.user
-            let admin = false
-            currentUser.roles.forEach(element => {
-              if (element.name === 'admin') {
-                admin = true
-                state.admin = true
-              }
-            })
-
-            if (trap) {
-              // Check if unmapped (no nz id) and if admin / pcord => Installation form
-              if (!trap.nz_trap_id && (pcord || admin)) {
-                router.push('/installform')
-              } else if (!trap.nz_trap_id) {
-                // Trap is unmapped and this isn't a pcord or admin
-                state.scanError = 'This QR code is unmapped, please contact the project coordinator or admin'
-              } else {
-                // Go to inspection form
-                router.push('/form')
-              }
-            } else {
-              state.scanError = 'Trap was not found with id: ' + data.qr_id
-            }
-          })
-        }).catch((error) => {
-          if (error.response) {
-            state.scanError = error.response.data.message
-          } else {
-            state.scanError = error.message
-          }
-        })
-      }
     },
     nearby (state, location) {
       // location = { lat: -41.307832, long: 174.756868 } // Debug
@@ -158,15 +105,100 @@ const scanModule = {
           break
         }
       }
+    },
+    setScanError (state, error) {
+      state.scanError = error
+    },
+    setScannedTrap (state, trap) {
+      console.log(trap)
+      state.scannedTrap = trap
+      state.scannedQRID = trap.qr_id
+    },
+    setPCordData (state, pcord) {
+      state.pcordData = pcord
+    },
+    setAdmin (state, value) {
+      state.admin = value
     }
   },
   actions: {
     scanQR ({ commit, rootState }, form) {
       form.user = rootState.auth.user
-      commit('scanQR', form)
       rootState.auth.user.roles.forEach(element => {
         if (element.name === 'admin') {
           commit('fetchNoCodes', null, { root: true })
+        }
+      })
+      // commit('scanQR', form)
+      return new Promise((resolve, reject) => {
+        const data = form
+        if (data.qr_id === null) {
+          // Dealing with an unmapped trap here (from location screen)
+          scan.checkPCordByTrap(data.id).then((response) => {
+
+          })
+            .catch(() => {
+              // User not a coordinator for this trap, shouldn't happen, abort aggresivly
+              // state.scanError = 'You don\'t have permission to do that'
+              commit('setScanError', 'You don\'t have permission to do that')
+              reject(Error('You don\'t have permission to do that'))
+            })
+        } else {
+          // QR exists, trap may not
+          scan.scan(data.qr_id).then((response) => {
+            const trap = response.data
+            // state.scannedTrap = trap
+            // state.scannedQRID = data.qr_id
+            commit('setScannedTrap', trap)
+            let pcord = false
+            scan.checkPCord().then((response) => {
+              pcord = true
+              commit('setPCordData', response.data)
+            }).catch(() => {
+              pcord = false
+            }).finally(() => {
+              const currentUser = data.user
+              let admin = false
+              currentUser.roles.forEach(element => {
+                if (element.name === 'admin') {
+                  admin = true
+                  // state.admin = true
+                  commit('setAdmin', true)
+                }
+              })
+
+              if (trap) {
+                // Check if unmapped (no nz id) and if admin / pcord => Installation form
+                if (!trap.nz_trap_id && (pcord || admin)) {
+                  router.push('/installform')
+                  resolve()
+                } else if (!trap.nz_trap_id) {
+                  // Trap is unmapped and this isn't a pcord or admin
+                  // state.scanError = 'This QR code is unmapped, please contact the project coordinator or admin'
+                  commit('setScanError', 'This QR code is unmapped, please contact the project coordinator or admin')
+                  reject(Error('This QR code is unmapped, please contact the project coordinator or admin'))
+                } else {
+                  // Go to inspection form
+                  router.push('/form')
+                  resolve()
+                }
+              } else {
+                // state.scanError = 'Trap was not found with id: ' + data.qr_id
+                commit('setScanError', 'Trap was not found with id: ' + data.qr_id)
+                reject(Error('Trap was not found with id: ' + data.qr_id))
+              }
+            })
+          }).catch((error) => {
+            if (error.response) {
+              // state.scanError = error.response.data.message
+              commit('setScanError', error.response.data.message)
+              reject(Error(error.response.data.message))
+            } else {
+              // state.scanError = error.message
+              commit('setScanError', error.message)
+              reject(Error(error.message))
+            }
+          })
         }
       })
     },
