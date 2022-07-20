c6d7ad87bfe54f3a8c823162df01d720e8db897f
apoliten <aponmino@gmail.com>
2022-07-21 00:27:49 +0300

    update password error message

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -74,7 +74,7 @@ export default {
     ],
     passwordRules: [
       (v) => !!v || 'Password is required',
-      (v) => v.length > 7 || 'Password must be at least 8 characters'
+      (v) => v.length > 7 || 'The password must be at least 8 characters'
     ],
     password_confirmationRules: [(v) => (v) => !!v || 'Passwords must match']
   }),
